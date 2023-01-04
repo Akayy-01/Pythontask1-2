513908741786db1bbb625378e8d7fc5d101b0588
Akayy-01 <121951765+Akayy-01@users.noreply.github.com>
2023-01-04 20:26:05 +0530

    Add files via upload

---
+++ postivenumbersinrange.py
@@ -0,0 +1,11 @@
+mylist=[12,-7,5,64,-14]
+for x in mylist:
+    if x>=0:
+        print("Output for list 1 is:",x)
+
+mylist=[12,14,-95,3]
+for x in mylist:
+    if x>=0:
+        print("Output for list 2 is:",x)
+        
+
